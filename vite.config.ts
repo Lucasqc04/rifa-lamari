ed13d339f306b45fc559978362e4b88c2a782093
lucasqc04 <lucasqcampos9@gmail.com>
2025-07-12 00:39:05 -0300

    add novos nomes

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src', // Diretório onde está o seu service worker
-      filename: 'firebase-messaging-sw.js', // Nome do arquivo processado
-      devOptions: {
-        enabled: true, // Habilita o sw em desenvolvimento (modo dev)
-        type: 'module',
-      },
-      manifest: {
-        name: 'Sua App',
-        short_name: 'App',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#3367D6',
-        icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-    }),
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
